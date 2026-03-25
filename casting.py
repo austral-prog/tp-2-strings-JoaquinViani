73404e00afe6257960a4e95c511fecb87cafff93
JoaquinViani <147179705+JoaquinViani@users.noreply.github.com>
2026-03-25 20:51:50 -0300

    Update casting.py

--- casting.py
+++ casting.py
@@ -3,11 +3,10 @@ def casting():
     descuento = float(input())
     cantidad = int(input())
 
-    precio_desc = precio * (1 - descuento / 100)
+    precio_desc = precio - (precio * descuento / 100)
     total = precio_desc * cantidad
 
     print(f"Precio: {precio}")
     print(f"Descuento: {descuento}")
     print(f"Precio con descuento: {precio_desc}")
     print(f"Total: {total}")
-
