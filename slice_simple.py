99aa508fcd84e0c9be44adca0134340e06f865c1
JoaquinViani <147179705+JoaquinViani@users.noreply.github.com>
2026-03-25 20:41:26 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,10 +1,6 @@
 def slice_simple():
-    """Dado el texto 'Awesome', imprime distintos substrings
-    usando slicing y lower().
-    """
     texto = "Awesome"
 
-    primero_3_caracteres = print(texto.lower()[0:3])
-    posicion_2_al_4 = print(texto.lower()[2:5])
-    completo = print(texto.lower()[0:])
-
+    print(texto.lower()[0:3])
+    print(texto.lower()[2:5])
+    print(texto.lower())
