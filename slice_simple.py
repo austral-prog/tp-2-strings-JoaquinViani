cad8d3dce73ff5d06ad27bb92a049ca66bef72bd
JoaquinViani <147179705+JoaquinViani@users.noreply.github.com>
2026-03-25 20:19:06 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,3 +3,8 @@ def slice_simple():
     usando slicing y lower().
     """
     texto = "Awesome"
+
+    primero_3_caracteres = print(texto.lower()[0:3])
+    posicion_2_al_4 = print(texto.lower()[2:5])
+    completo = print(texto.lower()[0:])
+
