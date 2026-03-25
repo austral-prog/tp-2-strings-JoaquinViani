685142fb5379a0b24f611172bcf1e0eb8dd9e21d
JoaquinViani <147179705+JoaquinViani@users.noreply.github.com>
2026-03-25 20:18:03 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,11 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+    
+    Nombre = input("Ingresa su nombre: ")
+    Apellido = input("Ingresa su apellido: ")
+
+    print(f"{Nombre.lower()} {Apellido.lower()}") 
+    print(f"{Nombre.title()} {Apellido.title()}") 
+    print(f"{Nombre.upper()} {Apellido.upper()}") 
+    print(f"    {Nombre.lower()} {Apellido.lower()}") 
