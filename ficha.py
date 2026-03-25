634af0fe304e7df661c186fcc573e1f4790f895c
JoaquinViani <147179705+JoaquinViani@users.noreply.github.com>
2026-03-25 20:16:59 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -24,4 +24,51 @@ def ficha():
     #   - Código secreto: nombre invertido en mayúsculas
     #   - Las 3 notas, su suma, promedio y promedio entero
     #   - Cierre decorativo usando repetición de string ("=" * 24)
-    pass
+
+    nombre_completo = input("Ingresa su nombre y apellido: ")
+    email = input("Ingresa su email: ")
+    nota_1 = int(input ("Ingresa tu primera nota: "))
+    nota_2 = int(input ("Ingresa tu segunda nota: "))
+    nota_3 = int(input ("Ingresa tu tercera nota: "))
+    nombre_limpio = nombre_completo.strip().title()
+    pos_espacio = nombre_limpio.find(" ")
+    iniciales = nombre_limpio[0] + nombre_limpio[pos_espacio + 1]
+    nombre_limpio = nombre_completo.strip().title()
+    nombre = nombre_limpio[:pos_espacio]
+    apellido = nombre_limpio[pos_espacio + 1:]
+    usuario = (apellido + "." + nombre).lower()
+    email_limpio = email.lower()
+    email_valido = "@" in email_limpio
+    pos_arroba = email_limpio.find("@")
+    dominio = email_limpio[pos_arroba + 1:]
+    nombre_archivo = nombre_limpio.replace(" ", "_")
+    cantidad_a = nombre_limpio.lower().count("a")
+    codigo = nombre_limpio[::-1].upper()
+    suma = nota_1 + nota_2 + nota_3
+    promedio = suma / 3
+    promedio_entero = suma // 3
+
+    
+    print("======================== \n  FICHA DEL ALUMNO\n========================")
+
+
+    print (f"Nombre: {nombre_completo.title()}")
+    print (f"Email: {email.lower()}")
+    print (f"Cantidad de caracteres: {len(nombre_completo)}")
+    print(f"Iniciales: {iniciales}")
+    print(f"Usuario: {usuario}")
+    print(f"Email valido: {email_valido}")
+    print(f"Dominio: {dominio}")
+    print(f"Nombre para archivo: {nombre_archivo}")
+    print(f"Cantidad de a: {cantidad_a}")
+    print(f"Codigo secreto: {codigo}")
+    print(f"Nota 1: {nota_1}")
+    print(f"Nota 2: {nota_2}")
+    print(f"Nota 3: {nota_3}")
+    print(f"Suma: {suma}")
+    print(f"Promedio: {promedio}")
+    print(f"Promedio entero: {promedio_entero}")
+    print("=" * 24)
+
+
+ficha()
