18c12ccbc509f159b2a3eacf072849d7fc155165
JoaquinViani <147179705+JoaquinViani@users.noreply.github.com>
2026-03-25 20:42:39 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,7 +1,3 @@
 def slice_advanced():
-    """Lee un texto e imprime los caracteres desde la posición 4
-    en adelante, tomando uno de cada dos (paso 2).
-    """
-
-    nombre = input("Ingresa una frase: ")
-    print(nombre[4::2])
+    texto = input()
+    print(texto[4::2])
